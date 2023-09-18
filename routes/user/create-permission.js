037ff7de653855f14942c9429570ed4e2e92cf91
maulikjsp <maulikd@jspinfotech.com>
2023-09-18 18:53:59 +0530

    update api

--- routes/user/create-permission.js
+++ routes/user/create-permission.js
@@ -3,14 +3,24 @@ const createPermission = async (req, res) => {
   try {
     const permissionName = req.body.permissionName;
 
-    const PermissionQuery =
-      await pool.query(`INSERT INTO permissions (permission_id, permission_name, permission_code) 
+    // Check if the examname or examcode already exists
+    const checkQuery = await pool.query(
+      "SELECT * FROM permissions WHERE permission_name = $1",
+      [permissionName]
+    );
+
+    if (checkQuery.rows.length > 0) {
+      return res.status(400).json({
+        message: "Duplicate permission . permission creation failed.",
+      });
+    }
+
+    await pool.query(`INSERT INTO permissions (permission_id, permission_name, permission_code) 
       VALUES ( (SELECT COALESCE(MAX(permission_id), 0) + 1 FROM permissions), '${permissionName}', LEFT('${permissionName}', 3) || '_' || RIGHT('${permissionName}', 3)
     );`);
-    const permission = PermissionQuery;
 
     return res.status(200).json({
-      permission: permission,
+      message: "Permission Added successfully",
     });
   } catch (error) {
     console.error("Error logging in:", error);

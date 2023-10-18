14ebf50d0e76f0b2706a4f9cee3b51825c852cfc
maulikjsp <maulikd@jspinfotech.com>
2023-10-18 12:10:17 +0530

    get exam from srta-uta

--- routes/exam/get-exams.js
+++ routes/exam/get-exams.js
@@ -1,13 +1,15 @@
-const { pool } = require("../../config/db");
+const { pool, poolUat } = require("../../config/db");
 
 const getExamData = async (req, res) => {
   try {
-    const examDataQuery = await pool.query("SELECT * FROM examdata");
+    const examDataQuery = await poolUat.query(`SELECT * FROM public."Exams"
+    ORDER BY "Id" ASC `);
     const examData = examDataQuery.rows;
     return res.status(200).json({
       exams: examData,
     });
   } catch (error) {
+    console.log(error, "error");
     return res.status(500).json({ message: "Internal Server error" });
   }
 };

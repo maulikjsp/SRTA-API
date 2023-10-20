ee0378c3a46734789d0fcb38e8feb9d59140afd7
maulikjsp <maulikd@jspinfotech.com>
2023-10-20 15:53:49 +0530

    update get exam api

--- routes/user/get-exams.js
+++ routes/user/get-exams.js
@@ -3,8 +3,21 @@ const getExams = async (req, res) => {
   try {
     const getExamsQuery = await pool.query("SELECT * FROM exams");
     const exams = getExamsQuery.rows;
+
+    // Create an array of promises for fetching users for each exam
+    const examDataWithUsersPromises = exams.map(async (item) => {
+      const usersQuery = await pool.query('SELECT * FROM "exam_user" WHERE "exam_id" = $1', [
+        item.id,
+      ]);
+      item.users = usersQuery.rows;
+      return item;
+    });
+
+    // Wait for all the promises to resolve
+    const examDataWithUsers = await Promise.all(examDataWithUsersPromises);
+
     return res.status(200).json({
-      exams: exams,
+      exams: examDataWithUsers,
     });
   } catch (error) {
     console.error("Error logging in:", error);

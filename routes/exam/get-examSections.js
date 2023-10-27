3fb1a5500c100418a49c6b1b55115c4c91ca1222
maulikjsp <maulikd@jspinfotech.com>
2023-10-27 15:23:10 +0530

    add get exam section query

---
+++ routes/exam/get-examSections.js
@@ -0,0 +1,24 @@
+const { pool, poolUat } = require("../../config/db");
+
+const getExamSections = async (req, res) => {
+  const { id, date } = req.params;
+  try {
+    const examDataQuery = await poolUat.query(
+      `SELECT "ExamSections"."ScheduleName", "ExamSections"."Id", "ExamSections"."ExamSectionTypeId",
+      "ExamSections"."ExamId", "ExamSectionTypes"."Name"
+FROM public."ExamSections"
+INNER JOIN public."ExamSectionTypes" ON "ExamSections"."ExamSectionTypeId" = "ExamSectionTypes"."Id"
+WHERE "ExamSections"."ExamId" = $1 AND "ExamSections"."ExamDate" = $2;
+      `,
+      [id, date]
+    );
+    const examData = examDataQuery.rows;
+    return res.status(200).json({
+      exams: examData,
+    });
+  } catch (error) {
+    console.log(error, "error");
+    return res.status(500).json({ message: "Internal Server error" });
+  }
+};
+module.exports = getExamSections;
--- routes/exam/index.js
+++ routes/exam/index.js
@@ -6,10 +6,12 @@ const createExam = require("./create-exam");
 const deleteExam = require("./delete-exam");
 const getExams = require("./get-exams");
 const updateExam = require("./update-exam");
+const getExamSections = require("./get-examSections");
 
 router.post("/create-exam", tokenVerification, createExam);
 router.get("/get-exams", tokenVerification, getExams);
 router.put("/update-exam", tokenVerification, updateExam);
 router.delete("/delete-exam/:id", tokenVerification, deleteExam);
+router.get("/get-exam-section/:id/:date", tokenVerification, getExamSections);
 
 module.exports = router;

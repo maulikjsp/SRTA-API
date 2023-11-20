ebdce7dcd76407315b4df39dec9e1a539a2a0817
maulikjsp <maulikd@jspinfotech.com>
2023-11-20 12:40:58 +0530

    update delete exam api

--- routes/exam/delete-exam.js
+++ routes/exam/delete-exam.js
@@ -3,7 +3,13 @@ const { pool } = require("../../config/db");
 const deleteExam = async (req, res) => {
   try {
     const id = req.params.id;
-    console.log(id, "id");
+    // find sections according exam and delete them
+    const sectionsQuery = await pool.query(`SELECT * FROM exam_sections WHERE exam_id=${id}`);
+    const sections = sectionsQuery.rows;
+    for (let i = 0; i < sections.length; i++) {
+      await pool.query(`DELETE FROM sections WHERE id=${sections[i]["section_id"]}`);
+      console.log("section deleted");
+    }
     await pool.query(`DELETE FROM exams WHERE id=${id}`);
     await pool.query(`DELETE FROM exam_user WHERE exam_id=${id}`);
     await pool.query(`DELETE FROM students WHERE exam_id=${id}`);

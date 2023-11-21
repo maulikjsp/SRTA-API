34a0a9081c527f13a9c55f0e18dda2f5ea351190
maulikjsp <maulikd@jspinfotech.com>
2023-11-21 17:25:23 +0530

    update procedure api

--- routes/procedure/create-procedure.js
+++ routes/procedure/create-procedure.js
@@ -3,7 +3,13 @@ const { pool } = require("../../config/db");
 const createProcedure = async (req, res) => {
   try {
     const { name, sections } = req.body;
-
+    // Check if the procedure already exists
+    const checkQuery = await pool.query("SELECT * FROM procedures WHERE title = $1", [name]);
+    if (checkQuery.rows.length > 0) {
+      return res.status(400).json({
+        message: "Procedure already exists",
+      });
+    }
     await pool.query(`INSERT INTO procedures (section_id, title) VALUES ($1, $2)`, [
       sections,
       name,

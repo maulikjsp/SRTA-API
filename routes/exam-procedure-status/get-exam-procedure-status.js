955f982dd49b0de9b79b74bc40f836ccd8d57251
maulikjsp <maulikd@jspinfotech.com>
2023-12-06 18:25:49 +0530

    update get exam procedure status

--- routes/exam-procedure-status/get-exam-procedure-status.js
+++ routes/exam-procedure-status/get-exam-procedure-status.js
@@ -3,7 +3,26 @@ const { pool } = require("../../config/db");
 const getExamProcedureStatus = async (req, res) => {
   try {
     // Retrieve all records from the table
-    const queryResult = await pool.query("SELECT * FROM exam_procedure_status");
+    const queryResult = await pool.query(`
+    SELECT
+    "exam_procedure_status"."id",
+    "exam_procedure_status"."student_id" AS "studentCode",
+    "exams"."active" AS "currentExamStatus",
+    "procedures"."title" AS "onGoingProcedure",
+    "users"."name" AS "currentAssignedExaminer",
+    "exam_procedure_status"."assigned_date" AS "AssignedDate",
+    "exam_procedure_status"."procedureid",
+    "exam_procedure_status"."examiner_id",
+    "exam_procedure_status"."exam_id",
+    "exams"."examname"
+FROM  
+    exam_procedure_status
+INNER JOIN 
+    "procedures" ON "procedures"."id" = "exam_procedure_status"."procedureid"
+INNER JOIN 
+    "exams" ON "exams"."id" = "exam_procedure_status"."exam_id"
+INNER JOIN
+    "users" ON "users"."id" = "exam_procedure_status"."examiner_id"`);
 
     // Extract the rows from the query result
     const records = queryResult.rows;

c4ba468929acd4c3b8324c6915e2eb6fda938640
maulikjsp <maulikd@jspinfotech.com>
2023-12-06 13:34:51 +0530

    update exam status api

--- routes/exam/update-exam-status.js
+++ routes/exam/update-exam-status.js
@@ -3,15 +3,16 @@ const { pool } = require("../../config/db");
 const statusChangeExam = async (req, res) => {
   try {
     const id = req.params.id;
+    const { status } = req.body;
     await pool.query(
       `
       UPDATE exams
   SET active = CASE
-    WHEN id = $1 THEN TRUE
+    WHEN id = $1 THEN $2
     ELSE FALSE
   END
       `,
-      [id]
+      [id, status]
     );
     return res.status(201).json({
       message: "Exam status changed successfully",

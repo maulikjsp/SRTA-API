69004165889e2ca8508b822c9b5b7d81cac288eb
maulikjsp <maulikd@jspinfotech.com>
2023-12-11 17:18:21 +0530

    update delete questionaires

--- routes/questionnaires/delete-questionnaires.js
+++ routes/questionnaires/delete-questionnaires.js
@@ -4,6 +4,8 @@ const deleteQuestionnaires = async (req, res) => {
   try {
     const id = req.params.id;
     await pool.query(`DELETE FROM questionnaires WHERE id = $1`, [id]);
+    await pool.query(`DELETE FROM criterias WHERE questionnaire_id = $1`, [id]);
+
     return res.status(201).json({ message: "Questionnaires deleted successfully" });
   } catch (error) {
     return res.status(500).json({ message: "Internal Server", error: error });

072e5306cba6eb17db4eee36eb0511f05d726b4e
maulikjsp <maulikd@jspinfotech.com>
2023-12-20 18:00:26 +0530

    add api for evaluation procedure

---
+++ routes/procedure/get-procedures-evaluation.js
@@ -0,0 +1,20 @@
+const { pool } = require("../../config/db");
+
+const getProcedureOfEvaluation = async (req, res) => {
+  try {
+    const procedureQuery = await pool.query(`SELECT procedures.*
+    FROM procedures
+    JOIN questionnaires ON procedures.id = questionnaires.procedure_id;
+     `);
+    const procedure = procedureQuery.rows;
+    return res.status(201).json({
+      procedure: procedure,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+module.exports = getProcedureOfEvaluation;
--- routes/procedure/index.js
+++ routes/procedure/index.js
@@ -5,9 +5,11 @@ const createProcedure = require("./create-procedure");
 const getProcedure = require("./get-procedure");
 const deleteProcedure = require("./delete-procedure");
 const updatedProcedure = require("./update-procedure");
+const getEvaluationProcedure = require("./get-procedures-evaluation");
 
 router.post("/create-procedure", tokenVerification, createProcedure);
 router.get("/get-procedures", tokenVerification, getProcedure);
+router.get("/get-evaluation-procedures", tokenVerification, getEvaluationProcedure);
 router.delete("/delete-procedure/:id", tokenVerification, deleteProcedure);
 router.put("/update-procedure", tokenVerification, updatedProcedure);
 module.exports = router;

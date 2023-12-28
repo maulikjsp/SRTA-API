580b6e48fcd98a373b65351f531da80c935294a0
maulikjsp <maulikd@jspinfotech.com>
2023-12-28 17:48:01 +0530

    update api for exam procedure status

--- routes/customquery/update-procedure-status.js
+++ routes/customquery/update-procedure-status.js
@@ -2,10 +2,10 @@ const { pool } = require("../../config/db");
 
 const updateExamProcedureStatus = async (req, res) => {
   try {
-    const { procedure_id, student_id, status } = req.body;
+    const { procedure_id, student_id, status, submited_examiner_id } = req.body;
     await pool.query(
-      `UPDATE exam_procedure_status SET status =$1 WHERE procedure_id =$2 AND student_id =$3`,
-      [status, procedure_id, student_id]
+      `UPDATE exam_procedure_status SET status = $1, submited_examiner_id = $4 WHERE procedure_id = $2 AND student_id = $3`,
+      [status, procedure_id, student_id, submited_examiner_id]
     );
     return res.status(200).json({ message: "procedure status updated" });
   } catch (error) {

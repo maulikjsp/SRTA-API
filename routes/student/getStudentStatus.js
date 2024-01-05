4d201fea7d7ae5058ce4f8a42805e2befb00134a
maulikjsp <maulikd@jspinfotech.com>
2024-01-05 16:56:28 +0530

    add examiner name in query

--- routes/student/getStudentStatus.js
+++ routes/student/getStudentStatus.js
@@ -3,18 +3,18 @@ const { pool } = require("../../config/db");
 const getStudentsStatus = async (req, res) => {
   try {
     const studentsDataQuery = await pool.query(`
-       SELECT "students"."name",
-"exam_procedure_status"."procedureid",
-"exam_procedure_status"."status",
-"procedures"."title",
-"exams"."examname",
-"users"."name"
-
-FROM "students"
-INNER JOIN "exam_procedure_status" ON "exam_procedure_status"."student_id" = "students"."id"
-INNER JOIN "procedures" ON "procedures"."id" = "exam_procedure_status"."procedureid"
-INNER JOIN "exams" ON "exams"."id" = "exam_procedure_status"."exam_id"
-LEFT JOIN  "users" ON "users"."id" = "exam_procedure_status"."examiner_id"
+    SELECT "students"."name",
+    "exam_procedure_status"."procedureid",
+    "exam_procedure_status"."status",
+    "procedures"."title",
+    "exams"."examname",
+    "users"."name" AS "examiner"
+    
+    FROM "students"
+    INNER JOIN "exam_procedure_status" ON "exam_procedure_status"."student_id" = "students"."id"
+    INNER JOIN "procedures" ON "procedures"."id" = "exam_procedure_status"."procedureid"
+    INNER JOIN "exams" ON "exams"."id" = "exam_procedure_status"."exam_id"
+    LEFT JOIN  "users" ON "users"."id" = "exam_procedure_status"."examiner_id"
        `);
     return res.status(200).json({ students: studentsDataQuery.rows });
   } catch (error) {

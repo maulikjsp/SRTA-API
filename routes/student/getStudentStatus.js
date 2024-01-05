5bac4789a0a0058fb003331726eae84dc374f085
maulikjsp <maulikd@jspinfotech.com>
2024-01-05 16:50:51 +0530

    add get students details query

--- routes/student/getStudentStatus.js
+++ routes/student/getStudentStatus.js
@@ -1,9 +1,25 @@
 const { pool } = require("../../config/db");
 
-const getStudentsStatus = async(req, res)=>{
-    try {
-       const studentsDataQuery = aw 
-    } catch (error) {
-        
-    }
-}
+const getStudentsStatus = async (req, res) => {
+  try {
+    const studentsDataQuery = await pool.query(`
+       SELECT "students"."name",
+"exam_procedure_status"."procedureid",
+"exam_procedure_status"."status",
+"procedures"."title",
+"exams"."examname",
+"users"."name"
+
+FROM "students"
+INNER JOIN "exam_procedure_status" ON "exam_procedure_status"."student_id" = "students"."id"
+INNER JOIN "procedures" ON "procedures"."id" = "exam_procedure_status"."procedureid"
+INNER JOIN "exams" ON "exams"."id" = "exam_procedure_status"."exam_id"
+LEFT JOIN  "users" ON "users"."id" = "exam_procedure_status"."examiner_id"
+       `);
+    return res.status(200).json({ students: studentsDataQuery.rows });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+};
+
+module.exports = getStudentsStatus;
--- routes/student/index.js
+++ routes/student/index.js
@@ -5,9 +5,11 @@ const { tokenVerification } = require("../../middleware");
 const updateStudent = require("./updateStudent");
 const getStudents = require("./getStudents");
 const getStudentSections = require("./getStudentSections");
+const getStudentsStatus = require("./getStudentStatus");
 
 router.put("/edit-student", tokenVerification, updateStudent);
 router.get("/get-students", tokenVerification, getStudents);
 router.get("/get-student-sections", tokenVerification, getStudentSections);
+router.get("/get-students-status", tokenVerification, getStudentsStatus);
 
 module.exports = router;

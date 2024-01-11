8023288a7115e28060c47a3387ba52730389e958
maulikjsp <maulikd@jspinfotech.com>
2024-01-11 15:28:22 +0530

    add api for get procedures for evaluation

---
+++ routes/evaluation/get-exam-procedure.js
@@ -0,0 +1,25 @@
+const { pool } = require("../../config/db");
+
+const getProcedures = async (req, res) => {
+  try {
+    const proceduresQuery = await pool.query(`
+    SELECT "procedures"."id","procedures"."title", "procedures"."section_id"
+    FROM "procedures"
+    INNER JOIN "exam_procedure_status" ON "exam_procedure_status"."procedureid" = "procedures"."id"
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM "procedure_submission"
+        WHERE "procedure_submission"."procedure_id" = "procedures"."id"
+        AND "exam_procedure_status"."status" = 'completed'
+    );    
+    `);
+
+    const procedures = proceduresQuery.rows;
+    return res.status(201).json({ procedures: procedures });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = getProcedures;
--- routes/evaluation/index.js
+++ routes/evaluation/index.js
@@ -4,8 +4,10 @@ const { tokenVerification } = require("../../middleware");
 
 const getQuestionairesByProcedureId = require("./get-questionnaires");
 const addExamResult = require("./add-exam-result");
+const getProcedures = require("./get-exam-procedure");
 
 router.get("/get-questionnairesById/:id", tokenVerification, getQuestionairesByProcedureId);
 router.post("/add-exam-result", tokenVerification, addExamResult);
+router.get("/get-procedures", tokenVerification, getProcedures);
 
 module.exports = router;

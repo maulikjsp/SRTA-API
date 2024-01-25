82cfdb0b21f29e8961a4ab80b7852a17af074e4c
maulikjsp <maulikd@jspinfotech.com>
2024-01-25 17:45:29 +0530

    add changes for delete exam procedure status

--- routes/exam-procedure-status/delete-exam-procedure.js
+++ routes/exam-procedure-status/delete-exam-procedure.js
@@ -3,9 +3,17 @@ const { pool } = require("../../config/db");
 const deleteExamProcedure = async (req, res) => {
   try {
     const id = req.params.id;
-
+    const getExmProcedure = await pool.query("SELECT * FROM exam_procedure_status WHERE id=$1", [
+      id,
+    ]);
+    const examProcedureData = getExmProcedure.rows;
     // Check if the exam procedure exists or not
     await pool.query("DELETE FROM exam_procedure_status WHERE id=$1", [id]);
+    await pool.query("DELETE FROM procedure_submission WHERE student_id=$1 AND procedure_id=$2", [
+      examProcedureData?.student_id,
+      examProcedureData?.procedureid,
+    ]);
+
     return res.status(200).json({
       message: "Procedure Deleted successfully",
     });

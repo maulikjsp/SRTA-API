285ab33ea4edaecd13bb6ca8b1bd8e4df6e071cb
maulikjsp <maulikd@jspinfotech.com>
2024-02-09 18:06:30 +0530

    add get escalated students api

--- routes/evaluation/get-escalated-students.js
+++ routes/evaluation/get-escalated-students.js
@@ -2,5 +2,32 @@ const { pool } = require("../../config/db");
 
 const getEscalatedStudent = async (req, res) => {
   try {
-  } catch (error) {}
+    const escalatedQuery = await pool.query(
+      `SELECT 
+      procedureid,
+      examiner_id,
+      student_id,
+      status,
+      escalated,
+      procedures.title AS procedure_name
+  FROM 
+      exam_procedure_status
+  INNER JOIN 
+      students ON exam_procedure_status.student_id = students.id
+  INNER JOIN 
+      procedures ON exam_procedure_status.procedureid = procedures.id
+  WHERE 
+      escalated = true;
+  `
+    );
+    return res.status(201).json({
+      procedure: escalatedQuery.rows,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
+
+module.exports = getEscalatedStudent;
--- routes/evaluation/index.js
+++ routes/evaluation/index.js
@@ -5,9 +5,11 @@ const { tokenVerification } = require("../../middleware");
 const getQuestionairesByProcedureId = require("./get-questionnaires");
 const addExamResult = require("./add-exam-result");
 const getProcedures = require("./get-exam-procedure");
+const getEscalatedStudent = require("./get-escalated-students");
 
 router.get("/get-questionnairesById/:id", tokenVerification, getQuestionairesByProcedureId);
 router.post("/add-exam-result", tokenVerification, addExamResult);
 router.get("/get-procedures", tokenVerification, getProcedures);
+router.get("/get-escalated-students", tokenVerification, getEscalatedStudent);
 
 module.exports = router;

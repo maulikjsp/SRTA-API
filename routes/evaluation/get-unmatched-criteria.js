788eaa66e82973f849a7169ea776b46a065e1d37
maulikjsp <maulikd@jspinfotech.com>
2024-02-10 15:31:11 +0530

    add changes for get unmatched criteria api

--- routes/evaluation/get-unmatched-criteria.js
+++ routes/evaluation/get-unmatched-criteria.js
@@ -3,10 +3,35 @@ const { pool } = require("../../config/db");
 const getUnmatchedCriteria = async (req, res) => {
   try {
     const { procedure_id, student_id } = req.body;
-    const escalatedQuery = await pool.query(`SELECT * FROM get_unmatched_criteria_list($1, $2)`, [
-      procedure_id,
-      student_id,
-    ]);
+    const escalatedQuery = await pool.query(
+      `SELECT
+    es.id,
+    es.procedure_id,
+    es.student_id,
+    es.criteria_id,
+    es.criteria_ans,
+    es.examiner_id,
+    es.questionnaires_id,
+    c.title,
+    u.name
+FROM
+    exam_submission es
+JOIN criterias c ON es.criteria_id = c.id
+JOIN users u ON es.examiner_id  = u.id
+WHERE
+    es.procedure_id = $1
+    AND es.student_id = $2
+    AND NOT EXISTS (
+        SELECT 1
+        FROM exam_submission es2
+        WHERE es2.procedure_id = $1
+          AND es2.student_id = $2
+          AND es2.criteria_id = es.criteria_id
+          AND es2.criteria_ans = es.criteria_ans
+          AND es2.id <> es.id
+    );`,
+      [procedure_id, student_id]
+    );
     return res.status(201).json({
       criteria: escalatedQuery.rows,
     });

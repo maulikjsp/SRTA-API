934cf160c81eedd2bb1566c11b34e37a6f9df528
maulikjsp <maulikd@jspinfotech.com>
2024-02-13 16:28:50 +0530

    add changes for escalation

--- routes/customquery/complete-procedure-status.js
+++ routes/customquery/complete-procedure-status.js
@@ -29,9 +29,7 @@ const completeExamProcedureStatus = async (req, res) => {
     );
 
     const completedCount =
-      statuses.filter((status) => status === "completed").length === statuses.length ||
-      (statuses.length != 1 &&
-        statuses.filter((status) => status === "completed").length === statuses.length - 1);
+      statuses.filter((status) => status === "completed").length === statuses.length;
 
     const escalated = statuses.filter((status) => status === "pending").length === 1;
 

91a5f4800bba9a18443d2e660b24d1229b546bd5
maulikjsp <maulikd@jspinfotech.com>
2024-02-14 10:07:57 +0530

    add changes for get students api

--- routes/student/getStudents.js
+++ routes/student/getStudents.js
@@ -2,7 +2,24 @@ const { pool } = require("../../config/db");
 
 const getStudents = async (req, res) => {
   try {
-    const studentsQuery = await pool.query("SELECT * FROM students");
+    const studentsQuery = await pool.query(`
+    SELECT 
+    "students"."sequence_number",
+    "students"."ext_student_id",
+    "students"."exam_id",
+    "students"."name",
+    "students"."email",
+    "students"."address",
+    "students"."phone",
+    "students"."school",
+    "students"."graduation_date",
+    "students"."surname",
+    "students"."social",
+    "students"."id"
+  FROM students
+  INNER JOIN exams ON "students"."exam_id" = "exams"."id"
+  WHERE active = true
+    `);
 
     const students = studentsQuery.rows;
 

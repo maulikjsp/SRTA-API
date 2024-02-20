a2ac675c94cc31a0cff81698b206a5aa7a3c2805
maulikjsp <maulikd@jspinfotech.com>
2024-02-20 15:52:26 +0530

    remove delete query for section and exam types into delete exam api

--- routes/exam/delete-exam.js
+++ routes/exam/delete-exam.js
@@ -5,16 +5,9 @@ const deleteExam = async (req, res) => {
     const id = req.params.id;
     // find sections according exam and delete them
     const sectionsQuery = await pool.query(`SELECT * FROM exam_sections WHERE exam_id=${id}`);
-    const sections = sectionsQuery.rows;
-    for (let i = 0; i < sections.length; i++) {
-      await pool.query(`DELETE FROM sections WHERE id=${sections[i]["section_id"]}`);
-      console.log("section deleted");
-    }
     await pool.query(`DELETE FROM exams WHERE id=${id}`);
     await pool.query(`DELETE FROM exam_user WHERE exam_id=${id}`);
     await pool.query(`DELETE FROM students WHERE exam_id=${id}`);
-    await pool.query(`DELETE FROM exam_sections WHERE exam_id=${id}`);
-    await pool.query(`DELETE FROM examtypes WHERE examid=${id}`);
 
     return res.status(201).json({ message: "Exam deleted successfully" });
   } catch (error) {

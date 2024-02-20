20aa77798824558aeb466556c1d9e99ffc5a79f0
maulikjsp <maulikd@jspinfotech.com>
2024-02-20 17:41:59 +0530

    add api for get all students

---
+++ routes/student/getAllStudents.js
@@ -0,0 +1,25 @@
+const { pool } = require("../../config/db");
+
+const getAllStudents = async (req, res) => {
+  try {
+    const studentsQuery = await pool.query(`
+    select 
+"students"."id",
+"students"."name",
+"students"."email",
+"exams"."examcode"
+from students
+inner join exams on "exams"."id" = "students"."exam_id"
+    `);
+
+    const students = studentsQuery.rows;
+
+    return res.status(200).json({
+      students: students,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+};
+
+module.exports = getAllStudents;
--- routes/student/index.js
+++ routes/student/index.js
@@ -6,10 +6,12 @@ const updateStudent = require("./updateStudent");
 const getStudents = require("./getStudents");
 const getStudentSections = require("./getStudentSections");
 const getStudentsStatus = require("./getStudentStatus");
+const getAllStudents = require("./getAllStudents");
 
 router.put("/edit-student", tokenVerification, updateStudent);
 router.get("/get-students", tokenVerification, getStudents);
 router.get("/get-student-sections", tokenVerification, getStudentSections);
 router.get("/get-students-status", tokenVerification, getStudentsStatus);
+router.get("/get-all-students", tokenVerification, getAllStudents);
 
 module.exports = router;

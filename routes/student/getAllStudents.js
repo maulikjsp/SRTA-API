be376a1c00743d2df70400a4806c92f6cc47b6dd
maulikjsp <maulikd@jspinfotech.com>
2024-02-28 16:43:05 +0530

    update get students query

--- routes/student/getAllStudents.js
+++ routes/student/getAllStudents.js
@@ -4,12 +4,13 @@ const getAllStudents = async (req, res) => {
   try {
     const studentsQuery = await pool.query(`
     select 
-"students"."id",
-"students"."name",
-"students"."email",
-"exams"."examcode"
-from students
-inner join exams on "exams"."id" = "students"."exam_id"
+    "students"."id",
+    "students"."name",
+    "students"."email",
+    "exams"."examcode",
+    "exams"."id" AS "exam_id"
+    from students
+    inner join exams on "exams"."id" = "students"."exam_id"
     `);
 
     const students = studentsQuery.rows;

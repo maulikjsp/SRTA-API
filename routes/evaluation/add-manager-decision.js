b0a548866cb1ecb05f5b0a71e7d2d0f1de06ea35
maulikjsp <maulikd@jspinfotech.com>
2024-03-07 14:50:47 +0530

    add changes for manger decision api

--- routes/evaluation/add-manager-decision.js
+++ routes/evaluation/add-manager-decision.js
@@ -2,44 +2,44 @@ const { pool } = require("../../config/db");
 
 const submitMangerDecision = async (req, res) => {
   try {
-    const {
-      procedure_id,
-      student_id,
-      manager_id,
-      criteria_id,
-      questionnaires_id,
-      questionnaires,
-      exam_procedure_id,
-      manager_decision,
-    } = req.body;
+    const { manager_id, exam_procedure_id } = req.body;
 
-    await pool.query(
-      `
-    INSERT INTO manager_decision (
-      procedure_id,
-      student_id,
-      manager_id,
-      criteria_id,
-      questionnaires_id,
-      questionnaires,
-      exam_procedure_id,
-      manager_decision
-  ) VALUES (
-      $1, $2, $3, $4, $5, $6, $7, $8
-  ) RETURNING *;
-    `,
-      [
-        procedure_id,
-        student_id,
-        manager_id,
-        criteria_id,
-        questionnaires_id,
-        questionnaires,
-        exam_procedure_id,
-        manager_decision,
-      ]
+    const queryMap = await Promise.all(
+      data?.questionnaires?.map(async (item, index) => {
+        const result = await pool.query(
+          `
+        INSERT INTO manager_decision (
+          procedure_id,
+          student_id,
+          manager_id,
+          criteria_id,
+          questionnaires_id,
+          questionnaires,
+          exam_procedure_id,
+          manager_decision
+      ) VALUES (
+          $1, $2, $3, $4, $5, $6, $7, $8
+      ) RETURNING *;
+        `,
+          [
+            item?.procedure_id,
+            item?.student_id,
+            manager_id,
+            item?.criteria_id,
+            item?.questionnaires_id,
+            { title: item?.title, ans: item?.ans },
+            exam_procedure_id,
+            item?.ans,
+          ]
+        );
+        return result.rows[0];
+      })
     );
-    if (manager_decision) {
+
+    if (
+      data?.questionnaires?.filter((item) => item?.ans === true).length ===
+      data?.questionnaires?.length
+    ) {
       const updateStatusQuery = `
       UPDATE exam_procedure_status
       SET status = $1, escalated = $3
